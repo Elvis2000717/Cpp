3ea55d50c5997144a521d0e75007d03c6be2295e
Elvis2000717 <1147445979@qq.com>
2021-07-09 21:44:20 +0800

    m_inst

---
+++ m_inst.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+//class Test
+//{
+//public:
+//	static Test& GetInst()
+//	{
+//		return m_inst;
+//	}
+//private:
+//	Test()
+//	{}
+//private:
+//	static Test m_inst;
+//};
+//
+//int main()
+//{
+//	Test &st = Test::GetInst();
+//
+//	return 0;
+//}
+
