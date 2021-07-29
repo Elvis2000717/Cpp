9400404811b69a184023de4c05462d3c35e12f4e
Elvis2000717 <1147445979@qq.com>
2021-07-29 18:39:05 +0800

    编辑距离

---
+++ 编辑距离.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+int minDistance(string& word1, string &word2) {
+	int len1 = word1.length();
+	int len2 = word2.length();
+
+	vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1));
+	for (int j = 0; j <= len2; j++)
+	{
+		dp[0][j] = j;
+	}
+	for (int i = 0; i <= len1; i++)
+	{
+		dp[i][0] = i;
+	}
+
+	for (int i = 1; i <= len1; i++)
+	{
+		for (int j = 1; j <= len2; j++)
+		{
+			dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1;
+			if (word1[i - 1] == word2[j - 1])
+			{
+				dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);
+			}
+			else
+			{
+				dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + 1);
+			}
+		}
+	}
+
+	return dp[len1][len2];
+}
+
+int main()
+{
+	string a = "horse";
+	string b = "ros";
+	int ret = minDistance(a, b);
+	cout << ret << endl;
+
+	return 0;
+}
